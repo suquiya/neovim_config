3d9fc6998e96a80ef59ed1c3cd52d1a79000c9ac
suquiya <22910980+suquiya@users.noreply.github.com>
2023-07-21 00:40:07 +0900

    init.lua update

---
+++ init.lua
@@ -0,0 +1,28 @@
+vim.loader.enable()
+
+vim.opt.tabstop=3
+vim.opt.shiftwidth=3
+-- Lazyのセットアップ（インストールできていなかったら取り寄せ）
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"williamboman/mason.nvim"
+	},
+		{"williamboman/mason-lspconfig.nvim"},
+		{"neovim/nvim-lspconfig"},
+},{
+	defaults={lazy=true}
+})
+
