d74ef55b63f81e78d094f96e1b25c96792813617
suquiya <22910980+suquiya@users.noreply.github.com>
2023-07-28 01:16:42 +0900

    rust-analyzer basic setup

--- init.lua
+++ init.lua
@@ -20,9 +20,42 @@ require("lazy").setup({
 	{
 		"williamboman/mason.nvim"
 	},
-		{"williamboman/mason-lspconfig.nvim"},
-		{"neovim/nvim-lspconfig"},
+	{
+		"williamboman/mason-lspconfig.nvim"
+	},
+	{
+		"neovim/nvim-lspconfig"
+	},
+	{
+		"simrat39/rust-tools.nvim"
+	}
 },{
 	defaults={lazy=true}
 })
 
+require("mason").setup();
+
+require("mason-lspconfig").setup({
+    ensure_installed = {
+		 "rust_analyzer"
+	 },
+});
+
+local rt = require("rust-tools")
+
+rt.setup({
+  tools = {
+  	 inlay_hints = {
+	 	parameter_hints_prefix = "←",
+		other_hints_prefix = "⇒",
+	 }
+  },
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
