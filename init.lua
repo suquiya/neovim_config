35cab1446e686d40a7c540898d33e67198bcd82f
suquiya <22910980+suquiya@users.noreply.github.com>
2023-08-13 00:49:52 +0900

    add completion settings

--- init.lua
+++ init.lua
@@ -2,47 +2,40 @@ vim.loader.enable()
 
 vim.opt.tabstop=3
 vim.opt.shiftwidth=3
+vim.g.mapleader=" "
 
 
-function key_map(mode,keys,to,_opts)
+local map = function(mode,keys,to,_opts)
 	local opts = {noremap = true,silent=false}
 	if _opts then 
 		opts = vim.tbl_extend('force',opts,_opts)
 	end
 	vim.keymap.set(mode,keys,to,opts)
 end
-local key_binds = {
-	{'i','jj','<Esc>'},
-	{'n','<C-s>',':w<CR>'},
-	{'i','<C-z>','<C-o>u'},
-	{'n','<C-z>','u'},
-	{'i','yy','<C-o>yy'},
-	{'i','pp','<C-o>p'},
-	--{},
-	--{},
-	--{},
-	--{},
-	--{},
-	--{},
-	--{},
-	--{},
-}
 
-for _i,bind in ipairs(key_binds) do
-	key_map(bind[1],bind[2],bind[3],bind[4])
-end
+map('i','jj','<Esc>')
+map('n','<C-s>',':w<CR>')
+map('i','<C-s>','<C-o>:w<CR>')
+map('i','<C-z>','<C-o>u')
+map('n','<C-z>','u')
+map('i','yy','<C-o>yy')
+map('i','<C-p>p','<C-o>p')
+map('n','<F3>',':noh<CR>')
+map('i','<F3>','<C-o>:noh<CR>')
+map('n','<leader>mo',':Mason<CR>')
+
 
 -- Lazyのセットアップ（インストールできていなかったら取り寄せ）
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+	"git",
+	 "clone",
+	"--filter=blob:none",
+	"https://github.com/folke/lazy.nvim.git",
+	"--branch=stable", -- latest stable release
+	lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -60,9 +53,6 @@ require("lazy").setup({
 		"simrat39/rust-tools.nvim"
 	},
 	{
-		"nvim-lua/plenary.nvim"
-	},
-	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
@@ -76,6 +66,7 @@ require("lazy").setup({
 					enable=true,
 				},
 			}
+			-- require 'nvim-treesitter.install'.prefer_git = false
 		end
 	},
 	{
@@ -83,7 +74,36 @@ require("lazy").setup({
    	event = "InsertEnter",
 		opts = {} -- this is equalent to setup({}) function
 	},
-	{ "lukas-reineke/indent-blankline.nvim" },
+	{
+		"lukas-reineke/indent-blankline.nvim"
+	},
+	{
+		'numToStr/Comment.nvim',
+		opts = {
+			-- add any options here
+		},
+	},
+	{
+		'nvim-lua/plenary.nvim',
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.2',
+		--dependencies = { 'nvim-lua/plenary.nvim' },
+		cmd="Telescope"
+	},
+	{
+		'nmac427/guess-indent.nvim',
+		cmd="GuessIndent",
+		config = function()
+			require('guess-indent').setup{}
+		end
+	},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/cmp-buffer'},
+	{'hrsh7th/cmp-path'},
+	{'hrsh7th/cmp-cmdline'},
+	{'hrsh7th/nvim-cmp'}
 },{
 	defaults={lazy=true}
 })
@@ -91,7 +111,7 @@ require("lazy").setup({
 require("mason").setup();
 
 require("mason-lspconfig").setup({
-    ensure_installed = {
+	ensure_installed = {
 		 "rust_analyzer"
 	 },
 });
@@ -99,18 +119,74 @@ require("mason-lspconfig").setup({
 local rt = require("rust-tools")
 
 rt.setup({
-  tools = {
-  	 inlay_hints = {
+	tools = {
+	 inlay_hints = {
 	 	parameter_hints_prefix = "←",
 		other_hints_prefix = "⇒",
-	 }
-  },
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+	 	}
+	},
+	server = {
+		on_attach = function(_, bufnr)
+			-- Hover actions
+			vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+			-- Code action groups
+		vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+		end,
+		server = {
+			settings = {
+				["rust-analyzer"] = {
+					checkOnSave = true,
+					check = {
+						command = "clippy",
+						features = "all"
+					}
+				}
+			}
+		}
+	},
+})
+
+
+vim.opt.list = true
+vim.opt.listchars:append "space:."
+vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "tab:--"
+vim.opt.listchars:append "trail:*"
+
+require("indent_blankline").setup {
+	-- for example, context is off by default, use this to turn it on
+	show_current_context = true,
+	show_current_context_start = true,
+}
+
+-- completion setup
+
+local cmp = require('cmp')
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+		end,
+	},
+	window = {
+		completion = {
+			cmp.config.window.bordered(),
+		},
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = {
+		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+		['<Tab>'] = cmp.mapping.select_next_item(),
+		['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.close(),
+		['<CR>'] = cmp.mapping.confirm { select = false }
+	},
+	sources = {
+		{ name = 'path' }, -- file paths
+		{ name = 'nvim_lsp', keyword_length = 1, priority = 10 }, -- from language server
+		{name="nvim_lsp"},
+		{name='buffer'}
+	})
 })
